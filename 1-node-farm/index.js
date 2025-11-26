b74a84427c0d79b9bb0419a521257e70b4eadda9
ashraf266 <mohamedashrafece@gmail.com>
2025-11-27 00:41:50 +0200

    day1

---
+++ 1-node-farm/index.js
@@ -0,0 +1,85 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+/////////////////////////////////////////////////////////////////////
+// FILES
+
+/*
+
+//sync blocking code
+const input = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(input);
+
+const textOut = `this is => ${input}.\ncreated in ${Date()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+
+//async non-blocking code
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) {
+    throw new Error(err.message);
+  } else {
+    console.log(data);
+  }
+});
+console.log("loading...");
+
+//callback Hell
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", data2 + "\n" + data3, "utf-8", (err) => {
+        console.log(err);
+        console.log("file has been written!!");
+      });
+    });
+  });
+});
+
+*/
+
+/////////////////////////////////////////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  //   console.log(req);
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    //important cuz ./ can result different directory if i run the node script elsewhere
+    //exception for that is the require function but generally don't use ./
+    // fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, fileText) => {
+    //   const productData = JSON.parse(fileText);
+    //   res.writeHead(200, {
+    //     "Content-type": "application/json",
+    //   });
+    //   res.end(fileText);
+    // });
+
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello World",
+    });
+    res.end("<h1>404 NOT FOUND!</h1>");
+  }
+  // res.end("Hello World!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
