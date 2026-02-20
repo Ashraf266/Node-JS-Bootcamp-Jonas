012a25c147d04a9241bd30bdc16795750f49c94c
ashraf <mohamedashrafece@gmail.com>
2026-02-20 05:06:46 +0200

    day13_1

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//Mounting Routers to a specific path
+// Mounting Routers to a specific path
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
